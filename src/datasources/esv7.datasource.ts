684c16deccb680565b4d5dfa251e9fe9e26fb910
ma7euus <mateuspastori@hotmail.com>
2020-09-10 22:06:57 -0300

    elasticsearch .env

--- src/datasources/esv7.datasource.ts
+++ src/datasources/esv7.datasource.ts
@@ -1,21 +1,13 @@
-import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 import config from './esv7.datasource.config';
 
-// Observe application's life cycle to disconnect the datasource when
-// application is stopped. This allows the application to be shut down
-// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
-// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
 export class Esv7DataSource extends juggler.DataSource
   implements LifeCycleObserver {
   static dataSourceName = 'esv7';
-  static readonly defaultConfig = config;
 
-  constructor(
-    @inject('datasources.config.esv7', {optional: true})
-    dsConfig: object = config,
-  ) {
-    super(dsConfig);
+  constructor() {
+    super(config);
   }
 }
