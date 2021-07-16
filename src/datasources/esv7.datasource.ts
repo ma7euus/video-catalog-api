befa44f46861b76c79b438e1f4598708e8af4a3d
ma7euus <mateuspastori@hotmail.com>
2021-07-15 23:27:47 -0300

    Testes e deploy produção

--- src/datasources/esv7.datasource.ts
+++ src/datasources/esv7.datasource.ts
@@ -1,15 +1,44 @@
-import {lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {
+    inject,
+    lifeCycleObserver,
+    LifeCycleObserver,
+    ValueOrPromise,
+} from '@loopback/core';
 import {juggler} from '@loopback/repository';
-import config from './esv7.datasource.config';
-
+import dbConfig from './esv7.datasource.config';
+import {Client} from 'es6';
 @lifeCycleObserver('datasource')
 export class Esv7DataSource extends juggler.DataSource
     implements LifeCycleObserver {
     static dataSourceName = 'esv7';
-    static readonly defaultConfig = config;
 
-    constructor() {
+    constructor(config = dbConfig) {
         super(config);
     }
 
+    /**
+     * Start the datasource when application is started
+     */
+    start(): ValueOrPromise<void> {
+        // Add your logic here to be invoked when the application is started
+    }
+
+    /**
+     * Disconnect the datasource when application is stopped. This allows the
+     * application to be shut down gracefully.
+     */
+    stop(): ValueOrPromise<void> {
+        return super.disconnect();
+    }
+
+    public async deleteAllDocuments() {
+        const index = (this as any).adapter.settings.index;
+        const client: Client = (this as any).adapter.db;
+        await client.delete_by_query({
+            index,
+            body: {
+                query: {match_all: {}},
+            },
+        });
+    }
 }
